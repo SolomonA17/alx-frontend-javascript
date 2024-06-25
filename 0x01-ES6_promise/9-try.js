38c8d927709fb8376875a9b466048da7503dd507
Solomon Aberra <solomonaberra75@gmail.com>
2024-06-25 09:17:24 -0700

    0x01-ES6_promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,14 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
     queue.push(mathFunction());
-  } catch (e) {
-    queue.push(e.toString());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push('Guardrail was processed');
+
   return queue;
 }
 
